52586967887c677697b12fed9e08b104d5bd00c8
esteban9706 <corlo.esteban@gmail.com>
2021-02-21 14:53:44 -0600

    Update MenuDigital.js

--- clientapp/src/components/MenuDigital.js
+++ clientapp/src/components/MenuDigital.js
@@ -1,46 +1,51 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route,Link } from "react-router-dom";
-import { Form, Card, Col, Row, Accordion, Badge, Container, Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { Form, Card, Col, Row, Accordion, Badge, Container, InputGroup } from 'react-bootstrap';
 import Picaña from './Menu/picana-t.jpg';
 import Papas from './papas-gajo.jpg';
+import SearchIcon from '@material-ui/icons/Search';
 
 import './Menu/menucliente.css'
 
 
 function MenuDigital() {
-    let history = useHistory();
+  let history = useHistory();
   return (
     <div className="menu-principal">
       <h1>La Noria</h1>
       <p>Mesa 1</p>
-      <span className="oi oi-basket"></span>
-      <Form.Control className="searchbar" size="sm" type="text" placeholder="Busca un platillo" />
+      <InputGroup>
+        <InputGroup.Prepend>
+          <InputGroup.Text id="basic-addon1"><SearchIcon /></InputGroup.Text>
+        </InputGroup.Prepend>
+        <Form.Control className="searchbar" type="text" placeholder="Busca un platillo" />
+      </InputGroup>
       <h2>Lo más recomendado</h2>
       <Link to={'/DetallePlatillo'}>
-      <Card className="mb-4" style={{ flex: 1 }}>
-        <Card.Img variant="top" src={Picaña} />
-        <Card.Body>
-          <Card.Title className="font-weight-bold">
-            <Row>
-              <Col>
-                Picaña Haye
-              </Col>
-              <Col>
+        <Card className="mb-4" style={{ flex: 1 }}>
+          <Card.Img variant="top" src={Picaña} />
+          <Card.Body>
+            <Card.Title className="font-weight-bold">
+              <Row>
                 <Col>
-                  <Badge className="badge" variant="primary"> El más pedido</Badge>
-                </Col>
+                  Picaña Haye
               </Col>
-            </Row>
-          </Card.Title>
-          <Card.Text>
-            Deliciosa Picaña Haye
+                <Col>
+                  <Col>
+                    <Badge className="badge" variant="primary"> El más pedido</Badge>
+                  </Col>
+                </Col>
+              </Row>
+            </Card.Title>
+            <Card.Text>
+              Deliciosa Picaña Haye
                         <br></br>
-            <span><span>$</span>365.00</span>
-          </Card.Text>
-        </Card.Body>
+              <span><span>$</span>365.00</span>
+            </Card.Text>
+          </Card.Body>
 
-      </Card>
+        </Card>
       </Link>
 
       <Accordion defaultActiveKey="0">
