5b39e6cc0d9282f7b464120d29fe32911f4fa57d
javier-bonilla <javier.bonilla@glovoapp.com>
2021-11-06 23:33:58 +0100

    Updates

--- Servidor/config/db.js
+++ Servidor/config/db.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require ('dotenv').config({path: 'variables.env'});
+require("dotenv").config({ path: "variables.env" });
 
-const conectarDB = async () =>{
-    try{
-        await mongoose.connect(process.env.DB_MONGO, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        });
-        console.log('DB Conectada')
-    }catch(error){
-        console.log(error);
-        process.exit(1); //Detener la app
-    }
-}
+const conectarDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://waitqradmin:34pitchRollYaw@clusteradmin.kunmc.gcp.mongodb.net/waitqr",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
+    console.log("DB Conectada");
+  } catch (error) {
+    console.log(error);
+    process.exit(1); //Detener la app
+  }
+};
 
 module.exports = conectarDB;
